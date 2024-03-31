5578f106a97880ed1048b2922743a2e3d8358325
Kamleshpc <Kamleshpc444@gmail.com>
2024-03-31 16:11:38 +0530

    initial Commit

---
+++ First.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// int main()
+// {
+//   std ::cout << "Hello Kamlesh!";
+//   return 0;
+// }
+
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+  puts("Click on button");
+  puts("create a new player");
+  puts("add life to player");
+  return 0;
+}
+  
