42d0b164150daf65f2471cea85c5d663663e0fe9
Kamleshpc <Kamleshpc444@gmail.com>
2024-04-01 15:00:35 +0530

    try a new code

--- First.cpp
+++ First.cpp
@@ -5,14 +5,19 @@
 //   return 0;
 // }
 
-#include <cstdio>
-using namespace std;
+// #include <cstdio>
+// using namespace std;
 
-int main()
-{
-  puts("Click on button");
-  puts("create a new player");
-  puts("add life to player");
+// int main()
+// {
+//   puts("Click on button");
+//   puts("create a new player");
+//   puts("add life to player");
+//   return 0;
+// }
+
+#include <iostream>
+int main (){
+  std :: cout<<"Hello There";
   return 0;
 }
-  
